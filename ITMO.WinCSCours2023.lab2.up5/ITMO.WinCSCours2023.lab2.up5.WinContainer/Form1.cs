79d6b32afd45944a4934f9b6778b2ce984132f64
AndrejSH1981 <133521547+AndrejSH1981@users.noreply.github.com>
2023-06-04 00:53:33 +0300

    Add files via upload

---
+++ ITMO.WinCSCours2023.lab2.up5/ITMO.WinCSCours2023.lab2.up5.WinContainer/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WinCSCours2023.lab2.up5.WinContainer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void but_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+                this.but.Text = "First";
+            else if (radioButton2.Checked == true)
+                this.but.Text = "Second";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.SetFlowBreak(button6, true);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button aButton = new Button();
+            tableLayoutPanel1.Controls.Add(aButton, 1, 1);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (splitContainer1.FixedPanel == FixedPanel.Panel1)
+                splitContainer1.FixedPanel = FixedPanel.None;
+            else
+                splitContainer1.FixedPanel = FixedPanel.Panel1;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            splitContainer1.IsSplitterFixed = !(splitContainer1.IsSplitterFixed);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            splitContainer1.Panel1Collapsed = !(splitContainer1.Panel1Collapsed);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+           /*panel1.AutoScroll = false;
+            panel1.HorizontalScroll.Enabled = true;
+            panel1.HorizontalScroll.Visible = true;
+            panel1.HorizontalScroll.Maximum = 0;
+            panel1.AutoScroll = true;*/
+        }
+    }
+}
