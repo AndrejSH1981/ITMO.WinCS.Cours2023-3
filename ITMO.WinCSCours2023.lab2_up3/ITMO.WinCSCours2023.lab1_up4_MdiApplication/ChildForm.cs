961617333ff50aefb7766998b48a2b3114406435
AndrejSH1981 <133521547+AndrejSH1981@users.noreply.github.com>
2023-06-04 00:51:27 +0300

    Add files via upload

---
+++ ITMO.WinCSCours2023.lab2_up3/ITMO.WinCSCours2023.lab1_up4_MdiApplication/ChildForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.WinCSCours2023.lab1_up4_MdiApplication
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+
+            InitializeComponent();
+        }
+
+        private void ToggleMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                //ParentForm.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+               // ParentForm.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+    }
+}
